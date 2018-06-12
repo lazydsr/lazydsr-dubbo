1229e33d278e56da189651600377dadcc3cd56a9
lazy <168367406@qq.com>
2018-06-12 23:54:32 +0800

    test

---
+++ lazydsr-dubbo-consumer/src/test/java/com/lazydsr/test/Test001.java
@@ -0,0 +1,20 @@
+package com.lazydsr.test;
+
+import java.lang.reflect.Array;
+
+/**
+ * Test001
+ * PROJECT_NAME: lazydsr-dubbo
+ * PACKAGE_NAME: com.lazydsr.test
+ * Created by Lazy on 2018/6/12 21:39
+ * Version: 0.1
+ * Info: @TODO:...
+ */
+public class Test001 {
+    public void test_1(){
+
+        //Array array = new Array();
+
+
+    }
+}
